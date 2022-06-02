c81b32ee49338947f10ea02833f0b476714b31a7
Thiago Viana Bezerra <Thiagovb62@gmail.com>
2022-06-02 12:57:28 -0300

    Organizando o carregamento dos modulos.

--- index.js
+++ index.js
@@ -2,14 +2,11 @@ import express from "express";
 import consign from "consign";
 import routes from "./routes";
 
-const PORT = 4000;
-
 const app = express();
 
-app.set("json spaces", 4);
-
-consign().include("models").then("routes").into(app);
-
-app.listen(PORT, () => {
-  console.log(`Ntask API listening on port = ${PORT}`);
-});
+consign()
+.include("models")
+.then("libs/middlewares.js")
+.then("routes")
+.then("libs/boot.js")
+.into(app);
